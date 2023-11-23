67b5e9d0df77b5aff53e8ebd09356945a8371935
South <91856822+mhmsouth@users.noreply.github.com>
2023-11-23 14:09:50 -0500

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,9 @@
+fx_version 'bodacious'
+games { 'gta5' }
+
+author 'Liberty Workshop - South'
+description 'KeepCalm AI Script'
+client_scripts {
+	"Common.Client.net.dll",
+	"KeepCalm.Client.net.dll",
+}
